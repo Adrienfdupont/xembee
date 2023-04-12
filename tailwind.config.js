7514132dad7a71adce594c75c2855f3cd128115f
Adrienfdupont <adrienf.dupont@gmail.com>
2023-04-12 22:08:47 +0200

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      "cooper-hewitt": ["Cooper-Hewitt", "sans-serif"],
+      "cooper-hewitt-light": ["Cooper-Hewitt-Light", "sans-serif"],
+      "cooper-hewitt-bold": ["Cooper-Hewitt-Bold", "sans-serif"],
+      azonix: ["Azonix", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
