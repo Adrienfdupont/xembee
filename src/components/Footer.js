72aba16e8ebd261dd89ab972e6cf44ebe4410792
Adrienfdupont <adrienf.dupont@gmail.com>
2023-04-15 12:52:00 +0200

    mentions légales

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import {BsFacebook, BsInstagram, BsLinkedin} from "react-icons/bs";
+import {Link} from "react-router-dom";
 
 export default function Footer() {
   return(
@@ -18,7 +19,7 @@ export default function Footer() {
         <div className="underline cursor-pointer">
           CONTACT@XEMBEE.COM
           <br/>
-          MENTIONS LÉGALES
+          <Link to="/legal-notices">MENTIONS LÉGALES</Link>
         </div>
 
         <div>
---
+++ src/pages/LegalNotices.js
@@ -0,0 +1,17 @@
+export default function LegalNotices() {
+  return(
+    <main>
+      <div className="container mx-auto px-2">
+        <p>
+          Toute représentation totale ou partielle de ce site et de son contenu, par quelques procédés que ce soient, sans autorisation préalable de la publication XEMBEE, est interdite et constituerait une contrefaçon sanctionnée par les articles L335-2 et suivants du Code de la Propriété Intellectuelle.
+          <br/>
+          <br/>
+          Protection des données personnelles:en attente
+          <br/>
+          <br/>
+          Conformément à la loi du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, le site a fait l'objet d'une déclaration auprès de la Commission Nationale Informatique et Libertés (CNIL). Les utilisateurs du site sont tenus de respecter les dispositions de la Loi Informatique, Fichiers et Libertés, dont la violation est passible de sanctions pénales. Ils doivent notamment s'abstenir, s'agissant des informations nominatives auxquelles ils accèdent, de toute collecte, de toute utilisation détournée, et d'une manière générale, de tout acte susceptible de porter atteinte à la vie privée ou à la réputation des personnes. Vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données vous concernant (loi " Informatique et Libertés " du 6 janvier 1978).
+        </p>
+      </div>
+    </main>
+  )
+}
